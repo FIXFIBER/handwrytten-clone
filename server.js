fca2f3a59a0e215ac7ca6e80b31b669f8df32200
FIXFIBER <fixfiber.team@gmail.com>
2026-08-20 20:13:55 +0100

    Add deployment configuration files

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+const PAGES_DIR = path.join(__dirname, 'pages');
+const ASSETS_DIR = path.join(__dirname, 'assets');
+
+app.use('/assets', express.static(ASSETS_DIR, { maxAge: '1d' }));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(PAGES_DIR, 'www.handwrytten.com', 'index.html'));
+});
+
+app.get('/integrations/integrate-automate', (req, res) => {
+  res.sendFile(path.join(PAGES_DIR, 'www.handwrytten.com', 'integrations', 'integrate-automate', 'index.html'));
+});
+
+app.use((req, res) => {
+  const urlPath = req.path;
+  const possiblePaths = [
+    path.join(PAGES_DIR, 'www.handwrytten.com', urlPath, 'index.html'),
+    path.join(PAGES_DIR, 'www.handwrytten.com', urlPath + '.html'),
+    path.join(PAGES_DIR, urlPath, 'index.html'),
+  ];
+  for (const p of possiblePaths) {
+    if (fs.existsSync(p)) return res.sendFile(p);
+  }
+  res.status(404).send('Page not found');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
